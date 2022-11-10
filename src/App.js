ad5f42e57b4fe716fcdb908b7b157c584e73030a
fadiramzi <fadiramzi.99@gmail.com>
2022-11-10 09:33:34 +0300

    ready

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = ()=>{
+  // State here
+  return (
+    <div className="App">
+      {/* Add new patient FORM */}
+      {/* Search input */}
+      {/* Table */}
+    </div>
+  );
+}
+
+export default App;
