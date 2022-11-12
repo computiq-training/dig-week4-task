c6e3b9a833c661ae3f5fd512872254927812bcf3
ALI ALOBAIDI <Aliwathik932@gmail.com>
2022-11-12 04:18:01 +0300

    VERY BAD STYLING

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
+import Patients from './components/patients';
+import {PATIENTS} from "./components/patients";
+import Search from './components/serach';
 const App = ()=>{
   // State here
   return (
     <div className="App">
+      <Patients/>
+        {/*<Search PATIENTS={PATIENTS}/>*/}
       {/* Add new patient FORM */}
       {/* Search input */}
       {/* Table */}
